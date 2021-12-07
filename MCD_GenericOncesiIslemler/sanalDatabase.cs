f4cc564529cd5f9578e3571b574bae9eb05f2b11
Faik Turan <turanfaik@hotmail.com>
2021-12-07 17:08:31 +0300

    Add project files.

---
+++ MCD_GenericOncesiIslemler/sanalDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    public class sanalDatabase
+    {
+        private ArrayList listem;
+
+        public sanalDatabase()
+        {
+            listem = new ArrayList();
+        }
+
+       public void yeniKayit(int data)
+        {
+            listem.Add(data);
+        }
+    }
+}
